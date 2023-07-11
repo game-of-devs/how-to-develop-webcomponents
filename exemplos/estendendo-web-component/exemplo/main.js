f1c03b4e93f38f5791bb04675c12222d8e5f0158
Techmista <45231624+techmist@users.noreply.github.com>
2023-07-11 13:44:08 -0300

    Update and rename exemplos/estendendo-web-component/main.js to exemplos/estendendo-web-component/exemplo/main.js

---
+++ exemplos/estendendo-web-component/exemplo/main.js
@@ -0,0 +1,35 @@
+class MyPostTable extends HTMLTableElement {
+    constructor() {
+        super();
+        this.init();
+        this.render();
+    }
+
+    init() {
+        this.thead = document.createElement('thead');
+        this.thead.innerHTML = `<tr><th>ID</th><th>Title</th></tr>`
+        this.append(this.thead);
+
+        this.tbody = document.createElement('tbody');
+        this.append(this.tbody);
+    }
+
+    render() {
+        this._request(this._populate)
+    }
+
+    _populate(json) {
+        for (const i in json) {
+            this.tbody.innerHTML += `<tr><td>${json[i]["id"]}</td><td>${json[i]["title"]}</td></tr>`
+        }
+    }
+
+    async _request(callback) {
+        const url = this.getAttribute("data-url");
+        let resp = await fetch(url)
+        return resp.json().then(callback);
+    }
+
+}
+
+customElements.define('my-post-table', MyPostTable, {extends: 'table'})
